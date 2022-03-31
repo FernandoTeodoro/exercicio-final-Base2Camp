8edd2f6542e5ae3a354c1c73290c12df1c77f9fb
Fernando Teodoro <fteodorodeoliveira@gmail.com>
2022-03-31 18:59:12 -0300

    Adição da bliblioteca Faker que auxilia na criação de informações para o preenchimento de alguns campos, exclusão do arquivo dataNewIssue e pequenas melhorias no código.

--- src/test/java/com/javaseleniumtemplate/tests/IssuesTests.java
+++ src/test/java/com/javaseleniumtemplate/tests/IssuesTests.java
@@ -1,16 +1,18 @@
 package com.javaseleniumtemplate.tests;
 
+import com.github.javafaker.Faker;
 import com.javaseleniumtemplate.bases.TestBase;
 import com.javaseleniumtemplate.pages.*;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import com.javaseleniumtemplate.GlobalParameters;
 
 
 public class IssuesTests extends TestBase {
 
-    //Metodo responsavel por fazer o login antes da execução de cada teste
+    //Método responsável por fazer o login antes da execução de cada teste
     @Before
     public void efetuarLogin(){
 
@@ -26,11 +28,15 @@ public class IssuesTests extends TestBase {
     }
 
     //Objects
+
+
     HomePage homePage;
     SelectProjectPage selectProjectPage;
     ReportIssuePage reportIssuePage;
     ReportedIssuePage reportedIssuesPage;
     ChangeStatusPage changeStatusPage;
+    Faker faker = new Faker();
+    String URL_DEFAULT = GlobalParameters.URL_DEFAULT;
 
     //Tests
     @Test
@@ -39,6 +45,7 @@ public class IssuesTests extends TestBase {
         homePage = new HomePage();
         selectProjectPage = new SelectProjectPage();
         reportIssuePage = new ReportIssuePage();
+
 
         /*
         MASSA DE DADOS:
@@ -49,31 +56,23 @@ public class IssuesTests extends TestBase {
         */
 
         //Parameters
-        String URL_CADASTRARISSUE = "https://mantis-prova.base2.com.br/bug_report_page.php";
-
-        //Dados necessarios para o cadastro de uma nova issue
         String nomeProjeto = "ProjectTatiane";
         String categoria = "[All Projects] General" ;
         String reprodutibilidade = "random";
         String severidade = "crash";
         String prioridade = "urgent";
         String plataforma = "PC Windows 10";
-        String resumo = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ex lectus, consequat at " +
-                        "efficitur in, dignissim a nisl.";
-        String descricao = "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin\n" +
-                           " literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney\n" +
-                           " College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage,\n" +                "            and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum\n" +
-                           " comes from sections 1.10.32 and 1.10.33 of \\\"de Finibus Bonorum et Malorum\\\" (The Extremes of Good and Evil)\n" +
-                           " by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance.\n" +
-                           " The first line of Lorem Ipsum, \\\"Lorem ipsum dolor sit amet..\\\", comes from a line in section 1.10.32.";
-        String passosParaRproducao = "1 - Dar o primeiro passo\n" +
-                                     "2 - Dar o segundo passo\n" +
-                                     "3 - Dar o terceiro passo\n" +
-                                     "4 - Repetir o primeiro passo\n" +
-                                     "5 - Se tudo der certo, vai dar errado aqui!";
+        String resumo = faker.harryPotter().quote();
+        String descricao = faker.lorem().characters(100,500);
+        String passosParaRproducao = "1- " + faker.harryPotter().character() + "\n" +
+                                     "2- " + faker.harryPotter().character() + "\n" +
+                                     "3- " + faker.harryPotter().character() + "\n" +
+                                     "4- " + faker.harryPotter().character() + "\n" +
+                                     "3- " + faker.harryPotter().character() + "\n" +
+                                     "3- " + faker.harryPotter().character() + "\n";
 
         //Test
-        homePage.navigateTo(URL_CADASTRARISSUE);
+        homePage.navigateTo(URL_DEFAULT + "bug_report_page.php");
 
         selectProjectPage.selecionarProjeto(nomeProjeto);
         selectProjectPage.clicarEmSelcionarProjeto();
@@ -108,12 +107,11 @@ public class IssuesTests extends TestBase {
         */
 
         //Parameters
-        String URL_ISSUE = "https://mantis-prova.base2.com.br/view.php?id=";
         String idIssue = "9073";
         String novoResponsavelPelaIssue = "Treinamento07";
 
         //Test
-        homePage.navigateTo(URL_ISSUE + idIssue);
+        homePage.navigateTo(URL_DEFAULT + "view.php?id=" + idIssue);
 
         reportedIssuesPage.selecionarResponsavel(novoResponsavelPelaIssue);
         reportedIssuesPage.clicarEmAdicionarResponsavel();
@@ -140,15 +138,13 @@ public class IssuesTests extends TestBase {
         */
 
         //Parameters
-        String URL_ISSUE = "https://mantis-prova.base2.com.br/view.php?id=";
-        String idIssue = "9072";
+        String idIssue = "9288";
         String novoStatus = "closed";
         String situacaoIssue = "not fixable";
-        String anotacao = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ex lectus, consequat at " +
-                          "efficitur in, dignissim a nisl.";
+        String anotacao = faker.harryPotter().quote();
 
         //Test
-        homePage.navigateTo(URL_ISSUE + idIssue);
+        homePage.navigateTo(URL_DEFAULT + "view.php?id=" +idIssue);
 
         reportedIssuesPage.selecionarNovoStatus(novoStatus);
         reportedIssuesPage.clicarEmAltrarStatus();
@@ -161,32 +157,7 @@ public class IssuesTests extends TestBase {
     }
 
     @Test
-    public void efetuarBuscaDeIssueInexistente(){
-        //Objects instances
-        homePage = new HomePage();
-        reportedIssuesPage = new ReportedIssuePage();
-
-        /*
-        MASSA DE DADOS:
-        Para a realização desse teste é necessario que exista um projeto ja cadastrado no sistema e o
-        ID de uma issue que não exista no sistema.
-
-        OBS.: Como no momento da criação desse script já existia um projeto cadastrado na plataforma eu
-        tomei a liberdade de usá-lo no meu teste e inventei um Id para poder utilizar no teste.
-        */
-
-        //Parameters
-        String idIssue = "100000";
-        String mensagemIssueNaoEncontrada = "Issue " + idIssue + " not found.";
-
-        homePage.preencherCampoIdIssue(idIssue);
-        homePage.clicarEmBuscarIssue();
-
-        Assert.assertEquals(reportedIssuesPage.retornaMensagemIssueNaoEncontrada(), mensagemIssueNaoEncontrada);
-    }
-
-    @Test
-    public void efetuarBuscadeIssueExistente(){
+    public void efetuarBuscaDeIssueExistente(){
         //Objects instances
         homePage = new HomePage();
         reportedIssuesPage = new ReportedIssuePage();
@@ -208,5 +179,29 @@ public class IssuesTests extends TestBase {
         Assert.assertEquals(reportedIssuesPage.retornaIdIssue(), "000"+idIssue);
     }
 
+    @Test
+    public void efetuarBuscaDeIssueInexistente(){
+        //Objects instances
+        homePage = new HomePage();
+        reportedIssuesPage = new ReportedIssuePage();
+
+        /*
+        MASSA DE DADOS:
+        Para a realização desse teste é necessario que exista um projeto ja cadastrado no sistema e o
+        ID de uma issue que não exista no sistema.
+
+        OBS.: Como no momento da criação desse script já existia um projeto cadastrado na plataforma eu
+        tomei a liberdade de usá-lo no meu teste e inventei um Id para poder utilizar no teste.
+        */
+
+        //Parameters
+        String idIssue = "1000000";
+        String mensagemIssueNaoEncontrada = "Issue " + idIssue + " not found.";
+
+        homePage.preencherCampoIdIssue(idIssue);
+        homePage.clicarEmBuscarIssue();
+
+        Assert.assertEquals(reportedIssuesPage.retornaMensagemIssueNaoEncontrada(), mensagemIssueNaoEncontrada);
+    }
 
 }
